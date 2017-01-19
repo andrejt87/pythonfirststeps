9d9cfcfc5cafa46a7abe8dc24bb749d6c4e61223
Andrej <andrej.tupikin@googlemail.com>
2017-01-19 19:38:50 +0100

    improvments: extract data from block read from goog

--- helloworld.py
+++ helloworld.py
@@ -1,15 +1,16 @@
-"""Plot High prices for IBM"""
-
-import pandas as pd
-import matplotlib.pyplot as plt
-
-def test_run():
-    df = pd.read_csv("meta/table.csv")
-    print df['High']
-    df['High'].plot()
-    plt.show()  # must be called to show plots
+import time, threading
+i = 0
+def test_run(i):
+    i = i + 1
+    print(time.ctime())
+    from googlefinance import getQuotes
+    import json
+    stock = getQuotes('AAPL')
+    print float(stock[0]['LastTradePrice'])
+    threading.Timer(3, test_run(i)).start()
 
 
 if __name__ == "__main__":
-    test_run()
+    
+    test_run(i)
 
