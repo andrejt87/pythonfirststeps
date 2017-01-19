6a21fc10bfda118af0eee0e8e2d9bb9fa9b5d49e
Andrej <andrej.tupikin@googlemail.com>
2017-01-19 22:26:42 +0100

    Time as x-axis added

--- helloworld.py
+++ helloworld.py
@@ -2,27 +2,48 @@ import time, threading
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mt
 import json
+from googlefinance import getQuotes
+from datetime import datetime
 
 i = 1
 price = []
 clock = []
+dates = []
 
  
 def test_run(i):
-    i = i + 1
+    
+    # print time
     print(time.ctime())
-    from googlefinance import getQuotes
+    
+    # get stock data
     stock = getQuotes('AAPL')
     price.append(float(stock[0]['LastTradePrice']))
+    
+    # increase indices
     clock.append(time.ctime())
-    submission = pd.DataFrame({'Price': price, 'Time': clock})
-    submission.to_csv('AAPL.csv', index=False)
+    i = i + 1
+    
+    dates.append(datetime.strptime(time.ctime() , "%a %b %d %H:%M:%S %Y"))
+    mt.dates.date2num(dates)
+    
+    # write to csv
+    csv_df = pd.DataFrame({'Price': price, 'Time': clock})
+    csv_df.to_csv('AAPL.csv', index=False)
+    
+    # plot data
     plt.ion()
-    plt.plot(price)
+    plt.plot(dates,price)
     plt.pause(1)
     print price
-    threading.Timer(3, test_run(i)).start()
+    
+    #reset timer
+    i = 0
+    
+    # cyclic function call
+    threading.Timer(1, test_run(i)).start()
     
 
 if __name__ == "__main__":
