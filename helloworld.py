1951c13e656a88978761c2f309800dbc3f31a710
Andrej <andrej.tupikin@googlemail.com>
2017-01-24 00:55:43 +0100

    step towards use of subfunctions

--- helloworld.py
+++ helloworld.py
@@ -13,35 +13,23 @@ dates = []
 
  
 def fetch_data(symbol):
-    while True:
-        # print time
-        print(time.ctime())
     
-        # get stock data
-        stock = getQuotes(symbol)
-        price.append(float(stock[0]['LastTradePrice']))
+    # get stock data
+    stock = getQuotes(symbol)
+    price.append(float(stock[0]['LastTradePrice']))
     
-        # increase indices
-        #clock.append(time.ctime())
-        clock.insert(0,time.ctime())
+    # increase indices
+    clock.insert(0,time.ctime())
     
-        dates.append(datetime.strptime(time.ctime() , "%a %b %d %H:%M:%S %Y"))
-        mt.dates.date2num(dates)
+    dates.append(datetime.strptime(time.ctime() , "%a %b %d %H:%M:%S %Y"))
+    mt.dates.date2num(dates)
     
-        # write to csv
-        csv_df = pd.DataFrame({'Price': price, 'Time': dates})
-        csv_df.to_csv("/Users/andrejtupikin/pythonfirststeps/" + str(symbol) + '.csv', index=False)
+    # write to csv
+    csv_df = pd.DataFrame({'Price': price, 'Time': dates})
+    csv_df.to_csv("/Users/andrejtupikin/pythonfirststeps/" + str(symbol) + '.csv', index=False)
     
-        # plot data
-        plotting(dates,price,symbol)
     
-        # print price
-        print price
-    
-        # cyclic function call
-        # threading.Timer(1, test_run(i)).start()
-    
-def plotting(x_val,y_val,symbol):
+def plotting():
 
     # plot data
     #plt.ion()
@@ -49,13 +37,34 @@ def plotting(x_val,y_val,symbol):
     axes_1.ticklabel_format(useOffset=False)
     #axes_1.set_title(str(y_val[-1]))
     axes_1.set_title(symbol)
-    axes_1.plot(x_val, y_val)
+    axes_1.plot(dates, price)
     #plt.annotate(str(symbol) + ":" + str(price[-1]), xy=(0.05, 0.95), xycoords='axes fraction')
+    print(time.ctime())
+    print price
     plt.pause(2)
     
-
-if __name__ == "__main__":
+    
+def processing():
+       fetch_data(sys.argv[1:]) 
+       
+def create_figure():
+    global figure_1
+    global axes_1
+    
     figure_1 = plt.figure()
     axes_1 = figure_1.add_subplot(111)
-    fetch_data(sys.argv[1:])
+
+def init():
+    global figure_1, axes_1, price, clock, dates, symbol
+    price = []
+    clock = []
+    dates = []
+    symbol = sys.argv[1:]
+
+if __name__ == "__main__":
+    init()
+    create_figure()
+    while True:
+        processing()
+        plotting()
 
